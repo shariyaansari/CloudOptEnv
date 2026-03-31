5316b7f534949231fb13923827247f6f075d0049
shariyaansari <shariyaansari8425@gmail.com>
2026-03-31 13:16:10 +0530

    First commit: OpenEnv Hackathon Submission

---
+++ server/app.py
@@ -0,0 +1,19 @@
+# server/app.py
+import uvicorn
+from openenv.core.env_server import create_fastapi_app
+from my_env.models import Action, Observation
+from my_env.env import CloudOptEnvironment
+import my_env.tasks.easy as task_module
+
+def main():
+    # Instantiate our environment
+    env = CloudOptEnvironment(task_module)
+    
+    # Wrap it in OpenEnv's standard FastAPI app
+    app = create_fastapi_app(env, Action, Observation)
+    
+    # Run the server
+    uvicorn.run(app, host="0.0.0.0", port=8000)
+
+if __name__ == "__main__":
+    main()
